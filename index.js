6c9ff1eb2f95c0f892cfeda3b05643b26fe7fd6f
Tom Coemans <tom.coemans@gmail.com>
2020-07-01 16:21:53 +0200

    initial code

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  Test: {
+    VlSearchResult: require('./test/e2e/components/vl-search-result.js'),
+    VlSearchResults: require('./test/e2e/components/vl-search-results.js'),
+  },
+};
---
+++ test/e2e/pages/vl-search-results.page.js
@@ -0,0 +1,18 @@
+const {Page, Config} = require('vl-ui-core').Test;
+const VLSearchResults = require('../components/vl-search-results');
+
+class VLSearchResultsPage extends Page {
+  async getSearchResults() {
+    return this._getSearchResults('vl-search-results');
+  }
+
+  async load() {
+    await super.load(Config.baseUrl + '/demo/vl-search-results.html');
+  }
+
+  async _getSearchResults(selector) {
+    return new VLSearchResults(this.driver, selector);
+  }
+}
+
+module.exports = VLSearchResultsPage;
