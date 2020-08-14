597739009692142b770fcd996e86548792139d8c
Tom Coemans <tom.coemans@gmail.com>
2020-08-14 16:54:40 +0200

    dist cache

---
+++ dist/vl-search-results-all.src.js
@@ -0,0 +1,8 @@
+import {VlSearchResult} from 'vl-ui-search-results/dist/vl-search-result.src.js';
+import {VlSearchResults} from 'vl-ui-search-results/dist/vl-search-results.src.js';
+
+export {
+  VlSearchResult,
+  VlSearchResults,
+};
+
