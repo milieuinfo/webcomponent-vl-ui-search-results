17c31cf865cb30a29a3dc0ef173f9bfaae6315d5
coemans <tom.coemans@vlaanderen.be>
2021-02-01 20:44:52 +0100

    update vl-ui-core dependency (#20)

--- dist/vl-search-results-all.src.js
+++ dist/vl-search-results-all.src.js
@@ -1,5 +1,5 @@
-import {VlSearchResult} from 'vl-ui-search-results/dist/vl-search-result.src.js';
-import {VlSearchResults} from 'vl-ui-search-results/dist/vl-search-results.src.js';
+import {VlSearchResult} from '../dist/vl-search-result.src.js';
+import {VlSearchResults} from '../dist/vl-search-results.src.js';
 
 export {
   VlSearchResult,
